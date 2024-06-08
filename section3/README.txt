202c23aa4d06c4fee1160b1204cc0d6e842e180e
Anshu Kulshrestha <anshu48k@gmail.com>
2024-06-08 20:12:58 +0530

    added code for all three microservices

---
+++ section3/README.txt
@@ -0,0 +1,3 @@
+This section covers the example of 
+Adding Open API dependecny
+Adding Open API documentation
---
+++ section3/loans/src/main/java/com/elearn/eazybank/loans/audit/AuditAwareConfig.java
@@ -0,0 +1,16 @@
+package com.elearn.eazybank.loans.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuditAwareConfig implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of("CARDS_MS");
+	}
+
+}
